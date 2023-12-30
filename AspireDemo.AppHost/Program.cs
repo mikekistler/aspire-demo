8cf39f853b4edef000b656dc0b0b6f259e65a5c9
Mike Kistler <mikekistler@microsoft.com>
2023-12-30 09:51:03 -0600

    Add aspire-starter template application

---
+++ AspireDemo.AppHost/Program.cs
@@ -0,0 +1,11 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.AspireDemo_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireDemo_Web>("webfrontend")
+    .WithReference(cache)
+    .WithReference(apiService);
+
+builder.Build().Run();
